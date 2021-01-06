65b6938be827aa4928f0f036fa9acee94b47b3d8
implementOne <micromouf@yahoo.fr>
2021-01-06 15:26:02 -0500

    Created Automatic Ecommerce Application

---
+++ deploy-ecommerce.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+STACK_NAME=awsecommerce
+REGION=us-east-1 
+CLI_PROFILE=awsecommerce1
+
+EC2_INSTANCE_TYPE=t2.micro
+
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+   # GitHubOwner=$GH_OWNER \
+   # GitHubRepo=$GH_REPO \
+   # GitHubBranch=$GH_BRANCH \
+   # GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+   # CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+
+
+       
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsecommerce1 \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
