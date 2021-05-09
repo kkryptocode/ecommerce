69dc390956baa4b539193244d4dee3908cf05c86
implementOne <micromouf@yahoo.fr>
2021-05-09 15:55:38 -0400

    reset

--- server/server.js
+++ server/server.js
@@ -5,18 +5,18 @@ import bidding from './controllers/bidding.controller'
 
 // Connection URL
 mongoose.Promise = global.Promise
-//mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true })
-//mongoose.connection.on('error', () => {
-  //throw new Error(`unable to connect to database: ${config.mongoUri}`)
-//})
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true })
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+})
 
 
 
 
 
-mongoose.connect("mongodb://kkmouf:123456@mongo:27017/?authSource=admin"
-).then(() => console.log("Successful")).catch((e) => console.log(e));
-const server = app.listen(config.port, (err) => {
+//mongoose.connect("mongodb://kkmouf:123456@mongo:27017/?authSource=admin"
+//).then(() => console.log("Successful")).catch((e) => console.log(e));
+//const server = app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
